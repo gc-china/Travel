3cdccdd21e2da9149968d6b426f9f52955acf8bb
“ <>
2019-12-30 23:18:50 +0800

    测试分支情况

--- travel/src/Pages/Home/components/Header.vue
+++ travel/src/Pages/Home/components/Header.vue
@@ -3,7 +3,7 @@
     <div class="header-left">
       <div class="iconfont back-icon">&#xe624;</div>
     </div>
-    <div class="header-input"><span class="iconfont">&#xe632;</span> 输入城市/景点/游玩主题</div>
+    <div class="header-input"><span class="iconfont">&#xe632;</span> 输入城市/游玩主题/景点</div>
     <div class="header-right arrow-icon">城市<span class="iconfont">&#xe64a;</span></div>
   </div>
 </template>
