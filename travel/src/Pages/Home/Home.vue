7faa91a8e7e12d32068a66324a66848ce8f5b0b4
“ <>
2020-01-01 21:51:13 +0800

    首页导航图标完成

--- travel/src/Pages/Home/Home.vue
+++ travel/src/Pages/Home/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
-   <home-header></home-header>
-   <home-swiper></home-swiper>
+    <home-header></home-header>
+    <home-swiper></home-swiper>
+    <home-icons></home-icons>
   </div>
 
 </template>
@@ -9,9 +10,11 @@
 <script>
     import HomeHeader from "./components/Header";
     import HomeSwiper from "./components/Swiper";
+    import HomeIcons from "./components/Icons";
+
     export default {
         name: "Home",
-        components: {HomeSwiper, HomeHeader},
+        components: {HomeIcons, HomeSwiper, HomeHeader},
     }
 </script>
 
---
+++ travel/src/Pages/Home/components/Icons.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="icons">
+    <div class="icon">
+      <div class="icon-img"><img class="icon-img-content"
+                                 src="https://picbed.qunarzz.com/f5e5770393d759578962e53ee67798c8.png " alt="">
+      </div>
+      <p class="icon-desc">名宿酒店</p>
+    </div><div class="icon">
+      <div class="icon-img"><img class="icon-img-content"
+                                 src="https://picbed.qunarzz.com/f5e5770393d759578962e53ee67798c8.png " alt="">
+      </div>
+      <p class="icon-desc">名宿酒店</p>
+    </div><div class="icon">
+      <div class="icon-img"><img class="icon-img-content"
+                                 src="https://picbed.qunarzz.com/f5e5770393d759578962e53ee67798c8.png " alt="">
+      </div>
+      <p class="icon-desc">名宿酒店</p>
+    </div><div class="icon">
+      <div class="icon-img"><img class="icon-img-content"
+                                 src="https://picbed.qunarzz.com/f5e5770393d759578962e53ee67798c8.png " alt="">
+      </div>
+      <p class="icon-desc">名宿酒店</p>
+    </div><div class="icon">
+      <div class="icon-img"><img class="icon-img-content"
+                                 src="https://picbed.qunarzz.com/f5e5770393d759578962e53ee67798c8.png " alt="">
+      </div>
+      <p class="icon-desc">名宿酒店</p>
+    </div><div class="icon">
+      <div class="icon-img"><img class="icon-img-content"
+                                 src="https://picbed.qunarzz.com/f5e5770393d759578962e53ee67798c8.png " alt="">
+      </div>
+      <p class="icon-desc">名宿酒店</p>
+    </div><div class="icon">
+      <div class="icon-img"><img class="icon-img-content"
+                                 src="https://picbed.qunarzz.com/f5e5770393d759578962e53ee67798c8.png " alt="">
+      </div>
+      <p class="icon-desc">名宿酒店</p>
+    </div><div class="icon">
+      <div class="icon-img"><img class="icon-img-content"
+                                 src="https://picbed.qunarzz.com/f5e5770393d759578962e53ee67798c8.png " alt="">
+      </div>
+      <p class="icon-desc">名宿酒店</p>
+    </div>
+    <div class="icon">
+      <div class="icon-img"><img class="icon-img-content"
+                                 src="https://picbed.qunarzz.com/f5e5770393d759578962e53ee67798c8.png " alt="">
+      </div>
+      <p class="icon-desc">名宿酒店</p>
+    </div>
+
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeIcons"
+    }
+</script>
+
+<style scoped lang="stylus">
+  @import '~styles/varibles.styl'
+  @import '~styles/mixins.styl'
+
+  .icons
+    padding-bottom 50%
+    overflow hidden
+    height 0
+
+    .icon
+      overflow hidden
+      float left
+      height 0
+      position relative
+      width 25%
+      padding-bottom 25%
+
+      .icon-img
+        position absolute
+        left 0
+        top 0
+        bottom .44rem
+        box-sizing border-box
+        padding .1rem
+
+        .icon-img-content
+          display block
+          margin 0 auto
+          height 100%
+
+      .icon-desc
+        position absolute
+        left 0
+        bottom 0
+        right 0
+        line-height .44rem
+        text-align center
+        color $darkTextColor
+        ellipsis()
+
+</style>
